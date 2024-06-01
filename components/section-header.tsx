754010862ddc16169e98c379ef2dad94cc67ddf6
Manoj Kumar <jalav@iu.edu>
2024-06-01 21:31:26 +0530

    section_header_added

--- components/section-header.tsx
+++ components/section-header.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Sectionheader({children}:{children:React.ReactNode}) {
     return (
-        <h2 className='text-bold text-2xl pb-3'>{children}</h2>
+        <h2 className='text-bold text-2xl my-8'>{children}</h2>
     );
 }
--- components/sectiondivider.tsx
+++ components/sectiondivider.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion'
 
 export default function Sectiondivider() {
   return (
-    <motion.div className='bg-gray-200 my-10 h-20 w-1 rounded-full hidden sm:block' initial={{y:-100, opacity :0}}
+    <motion.div className='bg-gray-200 mt-10 h-20 w-1 rounded-full hidden sm:block' initial={{y:-100, opacity :0}}
     animate={{y:0,opacity:1}}
     transition={{
         duration:1,
