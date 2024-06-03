8816e01acec78629699fcb36f1dd94e8a0f017c7
Manoj Kumar <jalav@iu.edu>
2024-06-03 13:33:25 +0530

    skills and project sections added

--- components/about-me.tsx
+++ components/about-me.tsx
@@ -1,16 +1,23 @@
 "use client"
-import React from 'react';
+import React, { useEffect } from 'react';
 import Sectionheader from './section-header';
 import { motion } from 'framer-motion'
-
+import { useInView } from 'react-intersection-observer';
+import { useActiveSectionContext } from '@/context/active-section';
+import useSectionInView from '@/lib/hooks';
 export default function Aboutme() {
+    const {ref }= useSectionInView('About')
+    
     return (
-        <motion.section className='text-center max-w-[40rem]' initial={{y:-100, opacity :0}}
+
+        <motion.section className='text-center max-w-[40rem] scroll-mt-20' initial={{y:-100, opacity :0}}
         animate={{y:0,opacity:1}} transition={{
                         type:'spring',
                         duration:1.25,
                         delay:2
-                    }}>
+                    }}
+                    
+                    id='about' ref={ref}>
             <Sectionheader>About Me</Sectionheader>
             <p className='leading-8'>
             I am a software development engineer with a strong foundation in programming and a passion for creating efficient and user-friendly software solutions. Currently pursuing a Master's in Computer Science at Indiana University Bloomington with a perfect GPA of 4.0, I specialize in developing and optimizing software using Java, Spring Boot, JavaScript, React.js, and Agile Methodologies.
--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,16 @@
-import React, { useRef } from 'react';
+"use client"
+import React, { useEffect } from 'react';
 import { projectsData } from '@/lib/data';
 import Sectionheader from './section-header';
 import Project from './project';
+import { useInView } from 'react-intersection-observer';
+import { useActiveSectionContext } from '@/context/active-section';
+import useSectionInView from '@/lib/hooks';
 
 export default function Projects() {
+    const {ref }= useSectionInView('Projects',0.25)
     return (
-       <section>
+       <section ref={ref} id='projects' className='scroll-mt-28'>
         <Sectionheader>My Projects</Sectionheader>
         <div>
             {projectsData.map((project,index)=>{
