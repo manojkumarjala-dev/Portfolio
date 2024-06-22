d419d1fc6c8fd257a3e33929e4eed2d3c816ba22
Manoj Kumar Jala <106336600+manojkumarjala-dev@users.noreply.github.com>
2024-06-22 12:40:35 -0400

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -70,7 +70,7 @@ export default function Intro() {
             <Link className='group bg-slate-900 rounded-full text-white px-6 py-2 flex items-center gap-2 outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition' href="#contact" onClick={()=>{setActiveSection("Contact");setTimeOfLastClick(Date.now())}}>Contact Me here <BsArrowRight className='group-hover:translate-x-1 transition'/></Link>
             <a className='group bg-gray-200 rounded-full text-black px-6 py-2 flex items-center gap-2 focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer' href='/Resume.pdf' download={true}>Download Resume <HiDownload className='group-hover:translate-y-1 transition'/></a>
             <a target='_blank' className='focus:scale-110 hover:scale-[1.15] hover:bg-gray-950 active:scale-105 transition' href='https://www.linkedin.com/in/manojkumarjala/'><Image src={linkedin} alt="LinkedIn" className='h-8 w-8' ></Image></a>
-            <a className='focus:scale-110 hover:scale-[1.15] hover:bg-gray-200 active:scale-105 transition rounded-full' href='https://github.com/manojmj98' target='_blank'><Image src={github} alt="Github" className='h-10 w-10 pt-1'></Image></a>
+            <a className='focus:scale-110 hover:scale-[1.15] hover:bg-gray-200 active:scale-105 transition rounded-full' href='https://github.com/manojkumarjala-dev' target='_blank'><Image src={github} alt="Github" className='h-10 w-10 pt-1'></Image></a>
         </motion.div>
 
     </section>
